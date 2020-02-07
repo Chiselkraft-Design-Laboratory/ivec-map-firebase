e59e86d936556cb6bf7f21fdbf6a18b88ea1a0c4
Ananth Narayanan <ananth@chiselkraft.in>
2020-02-07 06:55:10 +0530

    Dummies

---
+++ src/components/Dummies/index.js
@@ -0,0 +1,6 @@
+import ChargeStations from './chargestations.json';
+import ServiceStations from './servicestations.json';
+import Fleet from './fleet.json';
+import Cities from './cities.json';
+
+export { ChargeStations, ServiceStations, Fleet, Cities };
