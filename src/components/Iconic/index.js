3e1753fbcc248bc1bbdd386be61fa6b7b6e1202e
Ananth Narayanan <ananth@chiselkraft.in>
2020-03-01 17:35:57 +0530

    Iconic - vecmocon

--- src/components/Iconic/index.js
+++ src/components/Iconic/index.js
@@ -2,6 +2,7 @@ import ZoomInIcon from "./zoomin";
 import ZoomOutIcon from "./zoomout";
 import ZoomFitIcon from "./zoomfit";
 import IvecIcon from "./ivec";
+import VecmoconIcon from "./vecmocon";
 
 export { ZoomInIcon, ZoomOutIcon, ZoomFitIcon };
-export { IvecIcon };
+export { IvecIcon, VecmoconIcon };
