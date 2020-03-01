c4d1611bf7f84d354c651720021a3c01706ff718
Ananth Narayanan <ananth@chiselkraft.in>
2020-03-01 19:06:37 +0530

    Iconic - search

--- src/components/Iconic/index.js
+++ src/components/Iconic/index.js
@@ -1,8 +1,12 @@
 import ZoomInIcon from "./zoomin";
 import ZoomOutIcon from "./zoomout";
 import ZoomFitIcon from "./zoomfit";
+
 import IvecIcon from "./ivec";
 import VecmoconIcon from "./vecmocon";
 
+import SearchIcon from "./search";
+
 export { ZoomInIcon, ZoomOutIcon, ZoomFitIcon };
 export { IvecIcon, VecmoconIcon };
+export { SearchIcon };
